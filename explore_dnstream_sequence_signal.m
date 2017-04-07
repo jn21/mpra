54d1c4748e253b1cd7ce7e2c616f68f610142dba
Joseph Nasser <jnasser@broadinstitute.org>
2017-04-07 15:27:47 -0400

    Code reorg

--- explore_dnstream_sequence_signal.m
+++ explore_dnstream_sequence_signal.m
@@ -1,12 +1,17 @@
-function explore_dnstream_sequence_signal(signal,e_or_p_ratio,only_good_idx)
+function explore_dnstream_sequence_signal(signal,e_or_p_ratio,remove_reverse,save_fig)
 %
 % Input
 %       signal: (string) field from mpra_data_table: Eg dnstream_addPAS
 %       e_or_p_ratio: (string) which ratio to explore ('E_ratio_avg_rep' or
 %           'P_ratio_avg_rep')
-%       only_good_idx = remove reverse constructs?
+%       remove_reverse = (boolean) remove reverse constructs
 
 mpra_data = readtable('~/Documents/mpra/data/mpra_processed_data_with_annot.txt','Delimiter','\t');
+
+if remove_reverse
+    mpra_data = subset_table(mpra_data,'upstream_is_reverse',0);
+    mpra_data = subset_table(mpra_data,'dnstream_is_reverse',0);
+end
 
 signal_idx = logical(mpra_data{:,['dnstream_' signal]});
 signal_dnstream_ids = unique(mpra_data{signal_idx,'dnstream_full_id'});
@@ -20,10 +25,8 @@ for ii = 1:length(signal_dnstream_ids)
     
     %Get barcode data for constructs that have the signal and
     %their counterparts
-    temp_signal_idx = strcmp(mpra_data{:,'dnstream_full_id'},signal_dnstream_ids{ii});
-    temp_corr_idx = strcmp(mpra_data{:,'dnstream_full_id'},corresponding_dnstream_ids{ii});
-    temp_signal_table = mpra_data(temp_signal_idx,{'dnstream_full_id','upstream_full_id',e_or_p_ratio});
-    temp_corr_table = mpra_data(temp_corr_idx,{'dnstream_full_id','upstream_full_id',e_or_p_ratio});
+    temp_signal_table = subset_table(mpra_data,'dnstream_full_id',signal_dnstream_ids{ii});
+    temp_corr_table = subset_table(mpra_data,'dnstream_full_id',corresponding_dnstream_ids{ii});
     
     % remove infinite rows
     signal_is_finite_idx = isfinite(temp_signal_table{:,e_or_p_ratio});
@@ -33,14 +36,12 @@ for ii = 1:length(signal_dnstream_ids)
     
     %subset to common upstream ids
     common_upstream_ids = intersect(temp_signal_table{:,'upstream_full_id'},temp_corr_table{:,'upstream_full_id'});
-    temp_idx1 = ismember(temp_signal_table{:,'upstream_full_id'},common_upstream_ids);
-    temp_idx2 = ismember(temp_corr_table{:,'upstream_full_id'},common_upstream_ids);
+
+    temp_signal_table = subset_table(temp_signal_table,'upstream_full_id',common_upstream_ids);
+    temp_corr_table = subset_table(temp_corr_table,'upstream_full_id',common_upstream_ids);
     
-    temp_signal_table = temp_signal_table(temp_idx1,:);
-    temp_corr_table = temp_corr_table(temp_idx2,:);
-    
-    temp_signal_table = sortrows(temp_signal_table,2);
-    temp_corr_table = sortrows(temp_corr_table,2);
+    temp_signal_table = sortrows(temp_signal_table, 'upstream_full_id');
+    temp_corr_table = sortrows(temp_corr_table, 'upstream_full_id');
     
     full_signal_table = vertcat(full_signal_table,temp_signal_table);
     full_corr_table = vertcat(full_corr_table,temp_corr_table);
@@ -49,12 +50,21 @@ for ii = 1:length(signal_dnstream_ids)
         'error - ids not equal');
 end
 
-figure;
+switch e_or_p_ratio
+    case 'E_ratio_avg_rep'
+        e_or_p_ratio_str = 'Promoter Activity (E Barcode Ratio)';
+    case 'P_ratio_avg_rep'
+        e_or_p_ratio_str = 'Enhancer Activity (P Barcode Ratio)';
+end
+
+f = figure;
+set(f,'Units','normalized')
+set(f,'Position',[0 0 1 1])
 subplot(2,2,1)
 scatter(full_corr_table{:,e_or_p_ratio},full_signal_table{:,e_or_p_ratio})
 xlabel('no_signal','Interpreter','None')
 ylabel(signal,'Interpreter','None')
-title(e_or_p_ratio,'Interpreter','None')
+%title(e_or_p_ratio,'Interpreter','None')
 ax = gca;
 max_lim = max([ax.XLim ax.YLim]);
 min_lim = min([ax.XLim ax.YLim]);
@@ -65,19 +75,37 @@ ax.YLim = [min_lim max_lim];
 hold on
 plot([min_lim max_lim], [min_lim max_lim], 'r:')
 
-
 subplot(2,2,2)
 boxplot([full_signal_table{:,e_or_p_ratio} full_corr_table{:,e_or_p_ratio}],...
     'labels',{signal,'no_signal'})
+ylabel(e_or_p_ratio_str)
+grid on
 
-[p,h,stats] = signrank(full_signal_table{:,e_or_p_ratio},full_corr_table{:,e_or_p_ratio})
+[pval,~,~] = signrank(full_signal_table{:,e_or_p_ratio},full_corr_table{:,e_or_p_ratio});
 
 subplot(2,2,3)
-histogram(full_signal_table{:,e_or_p_ratio} - full_corr_table{:,e_or_p_ratio},...
+data = full_signal_table{:,e_or_p_ratio} - full_corr_table{:,e_or_p_ratio};
+med_data = median(data);
+histogram(data,...
     'BinWidth',.1)
-xlabel('signal - no_signal','Interpreter','None')
+hold on
+ax = gca;
+plot([med_data med_data], [0 ax.YLim(2)])
+xlabel([signal ' - no_signal'],'Interpreter','None')
 ylabel('histogram')
 
+title_str = sprintf(['%s \n',...
+    '%s \n',...
+    '%d constructs \n',...
+    'median absolute difference = %.2g \n', ...
+    'sign rank pvalue = %.2g'],...
+    signal,e_or_p_ratio_str,length(data),med_data,pval);
+suptitle(title_str)
+
+if save_fig
+    saveas(f,fullfile('~/Documents/mpra/fig/dnstream_signal/',[signal '_' e_or_p_ratio]),'png');
+    %print(f,'-r0','-dpng',fullfile('~/Documents/mpra/fig/dnstream_signal/',[signal '_' e_or_p_ratio]))
+end
 
 end
 
